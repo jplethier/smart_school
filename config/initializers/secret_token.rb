b59ce9944258d42f0610d3d3fad52499bb194d54
jplethier <jplethier@gmail.com>
2012-11-23 21:34:05 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartSchool::Application.config.secret_token = '0491a1c5cbb794d162d1c232d4058a39d18a918e7308046efe34ce01cd05e1d0f0c43392a033388d5c5b7825c23d53d560528edf9ec2dc7c1e73d389f270ce32'
