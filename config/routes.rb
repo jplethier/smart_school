276e7737187ea11660bdbc1e0288102b4afd072b
jplethier <jplethier@gmail.com>
2012-11-24 16:40:12 -0200

    criando rotas com namespaces para cada perfil de acesso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 SmartSchool::Application.routes.draw do
   devise_for :users
   
+  namespace :school do
+    root to: 'application#index'
+  end
+
+  namespace :teacher do
+    root to: 'application#index'
+  end
+
+  namespace :student do
+    root to: 'application#index'
+  end
+
+  namespace :admin do
+    root to: 'application#index'
+  end
+
+  root to: 'application#home'
 end
